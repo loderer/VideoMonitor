414f2f5cad7ab3e0cf7f0cd3b8ba603149f43dbe
Rudi Loderer <loderer@hm.edu>
2019-01-16 17:04:40 +0100

    Initial commit

---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+
+  public mediaDevices: Array<MediaStream>;
+
+  constructor() {
+    this.mediaDevices = [];
+    this.getMediaDevices();
+  }
+
+  ngOnInit() {
+  }
+
+getMediaDevices() {
+    var videoNumber = 0;
+    navigator.mediaDevices.enumerateDevices()
+     .then((devices) => {
+       devices.forEach((device) => {
+         if(device.kind === 'videoinput') {
+           var constraints = { deviceId: { exact: device.deviceId } };
+           var tmpVideoNumber = videoNumber;
+           var container = document.getElementById("container");
+           container.innerHTML += '<video id="video' + tmpVideoNumber + '" autoplay style="flex-grow:1;"></video>';
+           navigator.mediaDevices.getUserMedia({ video: constraints })
+           .then((stream) => {
+             var video = (<HTMLVideoElement> document.querySelector('#video' + tmpVideoNumber));
+             video.srcObject = stream;
+           });
+           videoNumber++;
+         }
+       });
+     })
+     .catch((err) => {
+       console.log(err.name + ": " + err.message);
+     });
+  }
+
+  logHelloWorld() {
+    console.log('HelloWorld!!!');
+  }
+}
